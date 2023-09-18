ce1e790454e9be8b363e232729deabaa0a38d446
danu <danujagreru@gmail.com>
2023-09-18 12:04:05 +0530

    ArithmeticException

---
+++ exception/src/unchecked/ArithmeticExceptionDemo.java
@@ -0,0 +1,26 @@
+package unchecked;
+
+/*
+    @author DanujaV
+    @created 9/18/23 - 11:54 AM   
+*/
+
+import java.util.Scanner;
+
+public class ArithmeticExceptionDemo {
+    public static void main(String[] args) {
+        int x = 10;
+
+        System.out.print("Input a number: ");
+        int y = new Scanner(System.in).nextInt();
+
+        try {
+            int result = x / y;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Hey user! you can't input 0 as a input");
+        }
+
+        System.out.println("End Main");
+    }
+}
